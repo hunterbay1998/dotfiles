a47f6c8c391c8130513db0acdc802471fddbea87
hunterbay1998 <bayjmay1998@gmail.com>
2026-04-03 14:24:15 +0100

    ags date and time new

--- .config/ags/widget/bar/Clock.tsx
+++ .config/ags/widget/bar/Clock.tsx
@@ -1,7 +1,8 @@
 import { createPoll } from "ags/time"
 
 export default function clock() {
-  const time = createPoll("",1000, "date '+%I:%M%p'")
+  const time = createPoll("",1000, "date '+%A %d %B %H:%M%p'")
+
 
   return <label label={time} />
-}
+} 
