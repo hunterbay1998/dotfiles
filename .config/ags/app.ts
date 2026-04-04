e603ab6d4b7d7654742af5c90db4427f56a3c304
hunterbay1998 <bayjmay1998@gmail.com>
2026-04-04 14:26:46 +0100

    need to sort out scss

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -1,8 +1,19 @@
 import app from "ags/gtk4/app"
+import { monitorFile } from "ags/file"
+import { exec } from "ags/process"
 import style from "./style/style.scss"
 import Bar from "./widget/Bar"
 import PowerMenu from "./widget/menu/PowerMenu"
 import BatteryMenu from "./widget/menu/BatteryMenu"
+
+const stylePath = `${SRC}/style`
+const scss = `${stylePath}/style.scss`
+const css = `/tmp/ags-style.css`
+
+function reloadCss() {
+  exec(`sass ${scss} ${css}`)
+  app.apply_css(css, true)
+}
 
 app.start({
   css: style,
@@ -10,5 +21,7 @@ app.start({
     app.get_monitors().map(Bar)
     app.get_monitors().map(PowerMenu)
     app.get_monitors().map(BatteryMenu)
-   },
+
+    monitorFile(stylePath, reloadCss)
+  },
 })
--- .config/ags/widget/bar/Battery.tsx
+++ .config/ags/widget/bar/Battery.tsx
@@ -21,6 +21,7 @@ const batteryIcon: Record<number, string> = {
 
 
 
+
 function getBatteryIcon(p: number) {
   const percent = Math.round(p * 100)
   const level = [100,90,80,70,60,50,40,30,20,10,0].find(t => percent >= t) ?? 0
@@ -42,10 +43,8 @@ export default function Battery() {
   const binding = createBinding(battery, "percentage")
   const formatPercent = (p: number) => `${Math.round(p * 100)}%`
   return (
-    <button onClicked={() => App.toggle_window("battery-menu")}>
-      <box cssClasses={binding.as(getBatteryClass)}> 
-        <label cssClasses={["batteryIcon"]} label={binding.as(getBatteryIcon)} />
-      </box>
+    <button cssClasses={binding.as(getBatteryClass)} onClicked={() => App.toggle_window("battery-menu")}>
+      <label label={binding.as(getBatteryIcon)} />
     </button>
   )
 }
