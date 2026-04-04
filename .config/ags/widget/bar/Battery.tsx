cf3e56336dd63cdf05c20e15c2bfbb58c4bfb05a
hunterbay1998 <bayjmay1998@gmail.com>
2026-04-04 10:44:26 +0100

    scss fix

--- .config/ags/widget/bar/Battery.tsx
+++ .config/ags/widget/bar/Battery.tsx
@@ -1,12 +1,8 @@
 //ags team made this for me to import
 import AstalBattery from "gi://AstalBattery"
-
 //will watch and update the label everytime the % changes 
-import { createBinding, createState } from "ags"
-
+import { createBinding } from "ags"
 // means the variable wont change 
-import { Gtk } from "ags/gtk4"
-
 const batteryIcon: Record<number, string> = {
   100: '󰁹',
   90:  '󰂂',
@@ -29,7 +25,6 @@ function getBatteryIcon(p: number) {
   return batteryIcon[level]
 }
 
-
 function getBatteryClass(p: number) {
     const percent = Math.round(p * 100)
     if (percent >= 80) return ["battery", "full"]
@@ -37,34 +32,16 @@ function getBatteryClass(p: number) {
     if (percent >= 40) return ["battery", "medium"]
     if (percent >= 20) return ["battery", "low"]
     return ["battery", "critical"]
-}
-
+  }
 
 
 export default function Battery() {
   const battery = AstalBattery.get_default()
   const binding = createBinding(battery, "percentage")
   const formatPercent = (p: number) => `${Math.round(p * 100)}%`
-  const [hovered, setHovered] = createState(false)
-  
-  
   return (
-    <box 
-       cssClasses={binding.as(getBatteryClass)}
-       $={(self) => {                              
-         const motion = new Gtk.EventControllerMotion()                   
-         motion.connect("enter", () => setHovered(true))                           
-         motion.connect("leave", () =>setHovered(false))                            
-         self.add_controller(motion)
-       }}                                              
-      >
+    <box cssClasses={binding.as(getBatteryClass)}>
       <label cssClasses={["batteryIcon"]} label={binding.as(getBatteryIcon)} />
-        <revealer
-           revealChild={hovered}
-           transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT}
-        >
-      <label cssClasses={["batteryPercentage"]} label={binding.as(formatPercent)} />
-        </revealer>
     </box>
   )
 }
--- .config/ags/widget/bar/Clock.tsx
+++ .config/ags/widget/bar/Clock.tsx
@@ -2,7 +2,6 @@ import { createPoll } from "ags/time"
 
 export default function clock() {
   const time = createPoll("",1000, "date '+%A %d %B %H:%M%p'")
-
-
-  return <label label={time} />
+  
+  return <label cssClasses={["clock"]} label={time} />
 } 
