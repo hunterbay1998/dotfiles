6b6dd14a6f93450af533ce8febdea766c42d8abb
hunterbay1998 <bayjmay1998@gmail.com>
2026-09-18 21:39:04 +0100

    more python practive with conditons and loops

--- python/test.py
+++ python/test.py
@@ -1,7 +1,26 @@
-names = ["bailey", "frank", "paula"]
+#starting the script or exit
+while True:
+    greeter = input("Hello Bailey, would you like to update? (yes/no) ").strip().lower()
 
-for name in names:
-    if name == "bailey":
-        print("bailey is in the list")
-    if name == "frank":
-        print("hello")
+    if greeter == "yes":
+        print("Let's update")
+        break
+    elif greeter == "no":
+        print("Okay, no update.")
+        break
+    else:
+        print("Please enter one of the options: yes or no")
+
+
+while True:
+    try:
+        days = int(input("how many days has it been since you last shaved your beard? "))
+    except ValueError:
+        print("please enter a full number e.g 1 or 2")
+    else:
+        break
+
+with open("beard_log.txt", "+a") as f:
+    f.write(f"{days}\n")
+
+print(f"saved {days} days without saving")
